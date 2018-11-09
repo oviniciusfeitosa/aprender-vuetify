7ba70abc7dfe0343f01bd934245dd99b39bc3898
vinnyfs89 <viniciusfesil@gmail.com>
2018-11-09 14:46:13 -0200

    [add] ADição do componente Entrar.vue

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Login from './views/Login.vue'
+import Entrar from './views/Entrar.vue'
 
 Vue.use(Router)
 
@@ -21,6 +22,11 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/entrar',
+      name: 'entrar',
+      component: Entrar
     },
     {
       path: '/login',
