8a207f10713841dfacc389b8c9efceb311955d21
vinnyfs89 <viniciusfesil@gmail.com>
2018-11-09 15:03:04 -0200

    [UPDATE] Ajuste de template básico de uma tela de login

--- src/views/Entrar.vue
+++ src/views/Entrar.vue
@@ -1,48 +1,40 @@
 <template>
-    <v-container>
-        Entrar
+    <v-container
+            grid-list-xl
+            fluid>
+        Olar
+
         <v-text-field
-            label="Usuario"
-            v-model="usuario"
-            :rules="nameRules"
-            :counter="10"
-            required
-            ></v-text-field>
+                v-model="name"
+                :rules="nameRules"
+                :counter="10"
+                label="Nome"
+                required
+        ></v-text-field>
         <v-text-field
-            label="Senha"
-            v-model="senha"
-            :rules="senhaRules"
-            required
-            ></v-text-field>
-        <v-btn
-            :disabled="!valid"
-            @click="submit"
-            >
-           entrar
-        </v-btn>
+                v-model="email"
+                :rules="emailRules"
+                :counter="10"
+                label="E-mail"
+                required
+        ></v-text-field>
     </v-container>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld'
-
-export default {
-  components: {
-    HelloWorld
-  },
-  data() {
+  export default {
+    name: 'Entrar',
+    data () {
       return {
-          valid: true,
-          nameRules: [ v => !! v || 'Não pode ser vazio!'],
-          senhaRules: [v => !! v || 'Senha não pode ser vazia!'],
-          usuario: null,
-          senha: null
+        nameRules: [
+          true
+        ],
+        name: '',
+        emailRules: [
+          true
+        ],
+        email: ''
       }
-  },
-  methods: {
-    submit() {
-        console.log('enviado!')
     }
   }
-}
 </script>
