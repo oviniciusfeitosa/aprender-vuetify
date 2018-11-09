824bbefdcc0190d08e5e58bae7146c15435fe488
vinnyfs89 <viniciusfesil@gmail.com>
2018-11-09 17:49:11 -0200

    [UPDATE] Utilização dos componente Eventos no componente Cadastro.

--- src/components/FormularioCadastro.vue
+++ src/components/FormularioCadastro.vue
@@ -44,60 +44,26 @@
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader'
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify'
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify'
-        }
-      ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com'
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com'
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuetifyjs.com'
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs'
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify'
-        }
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer'
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/layout/pre-defined'
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions'
-        }
+  import { mapActions, mapGetters } from 'vuex'
 
-      ]
-    })
+  export default {
+    data () {
+      return {
+        name: 'teste',
+        nameRules: [true],
+        date: '2018-03-02',
+        desc: '',
+        modal: false
+      }
+    },
+    methods: {
+      ...mapActions({ getEventos: 'eventos/eventos' }),
+      salvar () {
+        const params = { name: this.name, date: this.date, desc: this.desc }
+        this.getEventos(params)
+      },
+      allowedDates: val => parseInt(val.split('-')[2], 10) % 2 === 0
+    },
   }
 </script>
 
--- src/views/Cadastro.vue
+++ src/views/Cadastro.vue
@@ -7,30 +7,12 @@
 
 <script>
 
-  import { mapActions, mapGetters } from 'vuex'
+
   import Eventos from './Eventos'
   import FormularioCadastro from '../components/FormularioCadastro'
 
   export default {
     name: 'Cadastro',
-    components: { FormularioCadastro, Eventos },
-    data () {
-      return {
-        name: 'teste',
-        nameRules: [true],
-        date: '2018-03-02',
-        desc: '',
-        modal: false
-      }
-    },
-    methods: {
-      ...mapActions({ getEventos: 'eventos/eventos' }),
-      salvar () {
-        const params = { name: this.name, date: this.date, desc: this.desc }
-        this.getEventos(params)
-      },
-      allowedDates: val => parseInt(val.split('-')[2], 10) % 2 === 0
-    },
     // mounted () {
     //   this.getEventos({ name: 'lerolero', date: 'lerolero', desc: 'lerolero' })
     //
